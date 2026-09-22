59fad5a587c305791d21c3a1f199b453ff6885fa
CF8339816 <chris_french_3dga@hotmail.com>
2026-09-22 15:05:50 -0300

    post instructor review

--- Assets/Scripts/EventManager.cs
+++ Assets/Scripts/EventManager.cs
@@ -85,6 +85,7 @@ public class EventManager : MonoBehaviour
     void Update()
     {
         checkTimer();
+        SetItemsValue();
     }
 
     public void DisplayInfoMessage(string message)// formats info box messages to utalize display clear timer instead of being on screen dynamically
