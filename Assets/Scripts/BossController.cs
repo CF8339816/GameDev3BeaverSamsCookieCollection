918ebb72655a75a594a842bfcae0c7ca6d6cb296
RuinedVLR <rzhurakhov@gmail.com>
2026-09-22 10:16:33 -0300

    boss hand movement logic and pattern selection

--- Assets/Scripts/BossController.cs
+++ Assets/Scripts/BossController.cs
@@ -6,8 +6,21 @@ public class BossController : MonoBehaviour
 {
     [SerializeField] List<Transform> _jumpPositions = new List<Transform>();
     [SerializeField] List<Transform> _handPrepPosition = new List<Transform>();
+    [SerializeField] Transform _restPosition;
 
     [SerializeField] GameObject _bossHand;
+
+    [Header("Attack Timer Settings")]
+    [SerializeField] float _attackTimer = 0f;
+    [SerializeField] float _attackInterval = 5f;
+    [SerializeField] float _minAttackInterval = 2f;
+    [SerializeField] float _intervalChange = 0.5f;
+
+    [SerializeField] Transform _currentTarget;
+    [SerializeField] Transform _previousTarget;
+    [SerializeField] Transform _prepPos;
+
+    System.Random rand = new System.Random();
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,14 +31,48 @@ public class BossController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        _attackTimer += Time.deltaTime;
+
+        if (_attackTimer >= _attackInterval)
+        {
+            _attackTimer = 0f;
+            Attack();
+            if (_attackInterval > _minAttackInterval)
+            {
+                _attackInterval -= _intervalChange;
+            }
+        }
     }
 
     void Attack()
     {
-        Transform _currentTarget = _jumpPositions[UnityEngine.Random.Range(0, _jumpPositions.Count)];
-        Transform _prepPos = _handPrepPosition[UnityEngine.Random.Range(0, _handPrepPosition.Count)];
+        _prepPos = _handPrepPosition[rand.Next(0, _handPrepPosition.Count)];
+        _currentTarget = _jumpPositions[rand.Next(0, _jumpPositions.Count)];
 
         _bossHand.transform.position = _prepPos.position;
     }
+
+    /*void Attack()
+    {
+        int i;
+
+        if (_prepPos != null)
+        {
+            _previousTarget = _prepPos;
+            Debug.Log("set previous target");
+        }
+
+        
+        
+        while (_prepPos == _previousTarget && _prepPos != null)
+        {
+            Debug.Log("Trying to get the position");
+            i = rand.Next(0, _handPrepPosition.Count);
+            _prepPos = _handPrepPosition[i];
+            _currentTarget = _jumpPositions[i];
+        }
+
+        Debug.Log("Setting boss hand to selected position");
+        _bossHand.transform.position = _prepPos.position;
+    }*/
 }
